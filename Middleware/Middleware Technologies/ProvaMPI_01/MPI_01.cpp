1edaf0dc966215459bb71cc2f3d856a1d69fd8e3
Davide Bianchi <bianchi_davide@hotmail.it>
2015-01-09 11:51:30 +0100

    MPI creato

---
+++ Middleware/Middleware Technologies/ProvaMPI_01/MPI_01.cpp
@@ -0,0 +1,13 @@
+#include <mpi.h>
+#include <cstdio>
+
+int main(int argc, char* argv[]){
+	int rank, nproc;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+	printf("Hi, I'm n %d of %d\n", rank, nproc);
+	MPI_Finalize();
+	
+	return 0;
+}
