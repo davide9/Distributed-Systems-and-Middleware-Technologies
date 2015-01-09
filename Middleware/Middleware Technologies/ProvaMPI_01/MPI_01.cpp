c9e7128e110f8715322bc608a0ebf6043dab09f2
Davide Bianchi <bianchi_davide@hotmail.it>
2015-01-09 11:54:46 +0100

    Aggiunti Commenti

--- Middleware/Middleware Technologies/ProvaMPI_01/MPI_01.cpp
+++ Middleware/Middleware Technologies/ProvaMPI_01/MPI_01.cpp
@@ -4,7 +4,9 @@
 int main(int argc, char* argv[]){
 	int rank, nproc;
 	MPI_Init(&argc, &argv);
+	//ID del thread o processo
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	//Numero di thread o processi
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 	printf("Hi, I'm n %d of %d\n", rank, nproc);
 	MPI_Finalize();
