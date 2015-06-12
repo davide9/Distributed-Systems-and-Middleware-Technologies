67f89bbbafb8ec7d39859a9dadb1bcb2a4158efb
Davide Bianchi <bianchi_davide@hotmail.it>
2015-06-12 13:22:53 +0200

    lakedfjnh

--- Middleware/MovieBookWorm/routes/index.js
+++ Middleware/MovieBookWorm/routes/index.js
@@ -153,68 +153,73 @@ function httpMovieBookWorm(response){
 
 		res.on('data', function(piece) {
 			jsonStringResponseMovie += piece;
-			//console.log(jsonStringResponseMovie); %26
+			console.log(jsonStringResponseMovie); 
 		});
-
-		res.on('end', function() {
+		var i=0;
+		var countI=0;
+		res.on('end', function getBook() {
 			var contentMovie = JSON.parse(jsonStringResponseMovie);
 			
-			for(var i=0; i < contentMovie.total; i++){
-				queryTermBook = querystring.stringify({q:contentMovie.movies[i].title});
-				console.log('prestringify-> ' + contentMovie.movies[i].title);
-				console.log('stringify-> '  + queryTermBook);
+			queryTermBook = querystring.stringify({q:contentMovie.movies[i].title});
+			//console.log('prestringify-> ' + contentMovie.movies[i].title);
+			console.log('stringify-> '  + queryTermBook);
+		
+			var fullRequestQueryBook = partialQueryBook+queryTermBook+"&startIndex="+startIndex;
+  
+			console.log('Calling -> ' + fullRequestQueryBook);
+
+			var headersBook = {
+				'Content-Type': 'application/json'
+			};
+
+
+			var optionsBook = {
+				hostname: hostBook,
+				path: fullRequestQueryBook,
+				method: 'GET',
+				headers: headersBook
+			};
+
+			var jsonStringResponseBook = '';
+
+			var req = https.request(optionsBook, function(res) {
+				console.log("statusCode: ", res.statusCode);
+				console.log("headers: ", res.headers);
+
+				res.on('data', function(piece) {
+					jsonStringResponseBook += piece;
+					//console.log(jsonStringResponseBook);
+				});
+
+				res.on('end', function() {
+					var contentBook = JSON.parse(jsonStringResponseBook);
+					console.log('Found -> ' + contentBook.totalItems);
+					htmlBook ='';
+					htmlBook += '<h1>Movie' + contentMovie.movies[i].title + 'Search</h1>';
+					htmlBook += '<h3>Found -> ' + contentBook.totalItems + '</h3>';
+					htmlBook += '<h3>Showing -> ' + contentBook.items.length + '</h3>';
+					htmlBook += '<ul>';
+					for (var j = 0; j < contentBook.items.length; j++) {
+						htmlBook += '<li><a href="' + contentBook.items[j].volumeInfo.infoLink+ '">'+contentBook.items[j].volumeInfo.title+'</a></li>';
+					}
+					htmlBook += '</ul>';
+					console.log(htmlBook);
+					htmlMovie += htmlBook;
+					countI++;
+					if(i++ < contentMovie.movies.length)
+						getBook();
+					else
+						response.send(htmlMovie);
+				});
+
+			});
+
+			req.end();
+
+			req.on('error', function(e) {
+				console.error(e);
+			});
 			
-				var fullRequestQueryBook = partialQueryBook+queryTermBook+"&startIndex="+startIndex;
-	  
-				console.log('Calling -> ' + fullRequestQueryBook);
-
-				var headersBook = {
-					'Content-Type': 'application/json'
-				};
-
-
-				var optionsBook = {
-					hostname: hostBook,
-					path: fullRequestQueryBook,
-					method: 'GET',
-					headers: headersBook
-				};
-
-				var jsonStringResponseBook = '';
-
-				var req = https.request(optionsBook, function(res) {
-					console.log("statusCode: ", res.statusCode);
-					console.log("headers: ", res.headers);
-
-					res.on('data', function(piece) {
-						jsonStringResponseBook += piece;
-						//console.log(jsonStringResponseBook);
-					});
-
-					res.on('end', function() {
-						var contentBook = JSON.parse(jsonStringResponseBook);
-						console.log('Found -> ' + content.totalItems);
-						htmlBook ='';
-						htmlBook += '<h1>Movie' + contentMovie.movies[i].title + 'Search</h1>';
-						htmlBook += '<h3>Found -> ' + contentBook.totalItems + '</h3>';
-						htmlBook += '<h3>Showing -> ' + contentBook.items.length + '</h3>';
-						htmlBook += '<ul>';
-						for (var j = 0; j < contentBook.items.length; i++) {
-							htmlBook += '<li><a href="' + contentBook.items[j].infoLink+ '">'+contentBook.items[j].volumeInfo.title+'</a></li>';
-						}
-						htmlBook += '</ul>';
-						console.log(htmlBook);
-					});
-
-				});
-
-				req.end();
-
-				req.on('error', function(e) {
-					console.error(e);
-				});
-			}
-				response.send(htmlMovie);
 		});
 
 	});
